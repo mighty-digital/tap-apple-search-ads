5f811dccdf05b193f9fc3ef2fdbb6efd54418a0f
Yehor Borkov <yehor.borkov@gmail.com>
2021-12-04 14:28:06 +0200

    feat: Metadata serialization

--- src/tap_apple_search_ads/stream/metadata.py
+++ src/tap_apple_search_ads/stream/metadata.py
@@ -1,12 +1,18 @@
 from dataclasses import dataclass, field
-from typing import Tuple
+from typing import Any, Dict, Tuple
 
 
 @dataclass
 class Metadata:
-    enabled: bool = False
+    selected: bool = False
     breadcrumb: Tuple[str, ...] = field(default_factory=tuple)
 
     @classmethod
     def disabled(cls):
         return cls()
+
+    def dict(self) -> Dict[str, Any]:
+        return {
+            "metadata": {"selected": self.selected},
+            "breadcrumb": list(self.breadcrumb),
+        }
--- tests/stream/test_metadata.py
+++ tests/stream/test_metadata.py
@@ -3,5 +3,17 @@ from tap_apple_search_ads.stream import api
 
 def test_metadata_disabled():
     metadata = api.Metadata.disabled()
-    assert metadata.enabled is False
+    assert metadata.selected is False
     assert metadata.breadcrumb == tuple()
+
+
+def test_metadata_dict():
+    metadata = api.Metadata(True, ("properties", "test"))
+    expected = {"metadata": {"selected": True}, "breadcrumb": ["properties", "test"]}
+    assert metadata.dict() == expected
+
+
+def test_metadata_dict_disabled():
+    metadata = api.Metadata.disabled()
+    expected = {"metadata": {"selected": False}, "breadcrumb": []}
+    assert metadata.dict() == expected
