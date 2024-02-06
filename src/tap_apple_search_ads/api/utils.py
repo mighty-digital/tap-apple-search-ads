1bb23a7466878e1c668317318ce0861026a0bf94
Matthieu Caneill <matthieu.caneill@teampicnic.com>
2024-02-06 16:19:48 +0100

    Add URL when logging failing http requests

--- src/tap_apple_search_ads/api/utils.py
+++ src/tap_apple_search_ads/api/utils.py
@@ -6,8 +6,8 @@ logger = singer.get_logger()
 
 def check_response(response: requests.Response) -> bool:
     if response.status_code != 200:
-        message = "Received non-200 response code ([%s])"
-        logger.error(message, response.status_code)
+        message = "Received non-200 response code ([%s]) for URL %s"
+        logger.error(message, response.status_code, response.url)
         logger.error("Response content: [%s]", response.text)
 
         raise RuntimeError(message % response.status_code)
